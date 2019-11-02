c039f8907814c71f06bec8669b27fef632c98243
huhn <sehe89@gmail.com>
2019-11-02 21:15:50 +0100

    add cors with allowedOrigins

--- src/index.js
+++ src/index.js
@@ -33,27 +33,26 @@ if(process.env.NODE_ENV !== 'test') {
 
 
 
-var allowedOrigins = ['http://localhost:3002', "https://store.einfachiota.de"];
+var allowedOrigins = ["https://store.einfachiota.de", "https://eimag.einfachiota.de"];
 
-// Add headers
-app.use(function (req, res, next) {
+app.use(cors({
 
-	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	origin: function (origin, callback) {
+		// allow requests with no origin
+		// (like mobile apps or curl requests)
+		if (!origin) return callback(null, true);
+		if (allowedOrigins.indexOf(origin) === -1) {
+			var msg = 'The CORS policy for this site does not ' +
+				'allow access from the specified Origin.';
+			return callback(new Error(msg), false);
+		}
+		return callback(null, true);
+	},
 
-	// Request methods you wish to allow
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
 
-	// Request headers you wish to allow
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	res.setHeader('Access-Control-Allow-Credentials', true);
-
-	// Pass to next layer of middleware
-	next();
-});
+	credentials: true,
+}));
 
 app.use(bodyParser.urlencoded({
   extended: true
