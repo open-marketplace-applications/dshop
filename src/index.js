d1ff2d58eee933def0d90360f4dd2c293a06ee63
huhn <sehe89@gmail.com>
2019-11-02 18:56:15 +0100

    disable cors

--- src/index.js
+++ src/index.js
@@ -34,20 +34,7 @@ if(process.env.NODE_ENV !== 'test') {
 
 
 var allowedOrigins = ['http://localhost:3002', "https://store.einfachiota.de"];
-app.use(cors({
-	credentials: true,
-	origin: function (origin, callback) {
-		// allow requests with no origin 
-		// (like mobile apps or curl requests)
-		if (!origin) return callback(null, true);
-		if (allowedOrigins.indexOf(origin) === -1) {
-			var msg = 'The CORS policy for this site does not ' +
-				'allow access from the specified Origin.';
-			return callback(new Error(msg), false);
-		}
-		return callback(null, true);
-	}
-}));
+app.use(cors());
 
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
