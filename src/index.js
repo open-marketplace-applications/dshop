b78ba0c208352f4954d02d1f8de4d628992ad2cc
huhn <sehe89@gmail.com>
2019-11-02 19:24:39 +0100

    remove cors stuff

--- src/index.js
+++ src/index.js
@@ -36,12 +36,6 @@ if(process.env.NODE_ENV !== 'test') {
 var allowedOrigins = ['http://localhost:3002', "https://store.einfachiota.de"];
 app.use(cors());
 
-app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});
-
 app.use(bodyParser.urlencoded({
   extended: true
 }))
