1d6e5fdd156f526572be1abd2be251413f75dd84
huhn <sehe89@gmail.com>
2019-11-02 20:55:26 +0100

    fix headers

--- src/index.js
+++ src/index.js
@@ -39,9 +39,23 @@ app.use(cors({
 	exposedHeaders: config.corsHeaders
 }));
 
+// Add headers
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', 'https://eimag.einnfachIOTA.de');
+
+	// Request methods you wish to allow
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+	// Request headers you wish to allow
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader('Access-Control-Allow-Credentials', true);
+
+	// Pass to next layer of middleware
 	next();
 });
 
