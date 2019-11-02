2954eb816697f3482b052257d2e16935a5df4ff7
huhn <sehe89@gmail.com>
2019-11-02 21:11:54 +0100

    allow * in cors

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ var allowedOrigins = ['http://localhost:3002', "https://store.einfachiota.de"];
 app.use(function (req, res, next) {
 
 	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', 'https://eimag.einnfachIOTA.de');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 
 	// Request methods you wish to allow
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
