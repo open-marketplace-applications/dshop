0526b1864169c6e065c7423ad0b33fd8de3f9a0d
huhn <sehe89@gmail.com>
2019-11-02 20:52:52 +0100

    set headers

--- src/index.js
+++ src/index.js
@@ -39,6 +39,12 @@ app.use(cors({
 	exposedHeaders: config.corsHeaders
 }));
 
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.use(bodyParser.urlencoded({
   extended: true
 }))
