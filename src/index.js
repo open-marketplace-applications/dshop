6b787b1dc8158671719f46f4a7e9c10ff6ade0aa
huhn <sehe89@gmail.com>
2019-11-08 00:10:11 +0100

    add allowedOrigins and cors

--- src/index.js
+++ src/index.js
@@ -39,6 +39,30 @@ app.use(bodyParser.json({
 
 app.use(passport.initialize({ session: false }))
 
+var allowedOrigins = [];
+
+if (process.env.NODE_ENV == 'prod') {
+	allowedOrigins = ['https://magazin.einfachiota.de'];
+} else {
+	allowedOrigins = ['http://localhost:3000', 'http://localhost:5000'];
+}
+
+console.log("allowedOrigins");
+console.log(allowedOrigins);
+app.use(cors({
+	origin: function (origin, callback) {
+		// allow requests with no origin 
+		// (like mobile apps or curl requests)
+		if (!origin) return callback(null, true);
+		if (allowedOrigins.indexOf(origin) === -1) {
+			var msg = 'The CORS policy for this site does not ' +
+				'allow access from the specified Origin.';
+			return callback(new Error(msg), false);
+		}
+		return callback(null, true);
+	}
+}));
+
 const jwtOptions = {
 	secretOrKey: config.jwtSecret,
 	jwtFromRequest: ExtractJwt.fromHeader('authorization'),
