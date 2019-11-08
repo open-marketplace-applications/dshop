7a4c9b39266b438f6ec6e757157b1dd8c846f0a2
huhn <sehe89@gmail.com>
2019-11-08 01:51:57 +0100

    add paypal mode variable

--- src/api/payment.js
+++ src/api/payment.js
@@ -4,7 +4,7 @@ var paymentModule = require('iota-payment')
 var paypal = require('paypal-rest-sdk');
 
 paypal.configure({
-    'mode': 'sandbox', //sandbox or live
+    'mode': process.env.PAYPAL_MODE, // 'sandbox' or 'live'
     'client_id': process.env.PAYPAL_CLIENT_ID,
     'client_secret': process.env.PAYPAL_CLIENT_SECRET
 });
