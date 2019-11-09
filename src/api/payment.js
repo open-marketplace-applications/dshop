f653b23d189b45976aba5f64696ed7787c9bfb74
huhn <sehe89@gmail.com>
2019-11-09 02:38:38 +0100

    add old payment module

--- src/api/payment.js
+++ src/api/payment.js
@@ -33,7 +33,8 @@ api.post('/pay_with_iota', (req, response) => {
                     obj.info = {}
                     obj.info.live_price = live_price
                     obj.info.timestamp = Date.now() / 1000 | 0
-                    paymentModule.payment.createPayment(iota_price, obj).then(payment => {
+                    iota_price = 1
+                    paymentModule.payments.createPayment(iota_price, obj).then(payment => {
                         const obj = {
                             order,
                             payment: {
