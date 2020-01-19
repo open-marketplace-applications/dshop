7192928170f20ed7114b25dd1be53ff391134bde
huhn511 <sehe89@gmail.com>
2020-01-19 22:36:44 +0100

    removed test price

--- src/api/payment.js
+++ src/api/payment.js
@@ -34,7 +34,6 @@ api.post('/pay_with_iota', (req, response) => {
                     obj.info = {}
                     obj.info.live_price = live_price
                     obj.info.timestamp = Date.now() / 1000 | 0
-                    iota_price = 1
                     paymentModule.payments.createPayment(iota_price, obj).then(payment => {
                         const obj = {
                             order,
