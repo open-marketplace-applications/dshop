f50248872abbf038e764c656eb3cb9e981bf7985
huhn511 <sehe89@gmail.com>
2020-01-31 23:48:49 +0100

    formatting

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -14,61 +14,63 @@ Order.setPayed = function (order) {
 
     console.log('setOrderPayed - order', order)
 
-    Order.findOne({ _id: order._id }, function(err, order) {
-        if(!err) {
-            if(!order) {
+    Order.findOne({ _id: order._id }, function (err, order) {
+        if (!err) {
+            if (!order) {
                 // there is no order with this id
                 console.log('setOrderPayed no order found.')
             } else {
                 order.status = 'payed';
-                order.save(function(err) {
-                    if(!err) {
+                order.save(function (err) {
+                    if (!err) {
                         console.log("order payed ", order._id);
                         console.log('order - item', order)
 
-                        if(process.env.NODE_ENV == 'prod') {
+                        if (process.env.NODE_ENV == 'prod') {
                             // TODO: Add i18n 
                             var mailOptions = { from: 'no-reply@einfachIOTA.de', to: order.email, subject: 'The einfachIOTA team has received your payment.', text: 'Hello ' + order.first_name + ',\n\n' + 'Thank you for the purchase. We hope you enjoy reading it,' + '\n' + 'Your einfachIOTA Team.' };
-                            transporter.sendMail(mailOptions, function(err) {
+                            transporter.sendMail(mailOptions, function (err) {
                                 if (err) { console.log("Error sending mail.", err) }
                                 console.log("Paymend success message sent to: ", order.email)
                             });
                         }
 
-                        let hock_name = 'eiMAG'
-                        if(process.env.NODE_ENV == 'dev') {
-                            hock_name = 'eiMAG_dev'   
-                        }
-                        let hock_content = 'Verkauft! Menge: ' + order.amount
+                        // Handle Webhook notification
+                        if (process.env.WEBHOOK_URL) {
+                            let hock_name = 'eiMAG'
+                            if (process.env.NODE_ENV == 'dev') {
+                                hock_name = 'eiMAG_dev'
+                            }
+                            let hock_content = 'Verkauft! Menge: ' + order.amount
 
-                        // Send Webhook notification
-                        axios.post(process.env.WEBHOOK_URL, {
-                            content: hock_content,
-                            username: hock_name
-                          })
-                          .then(function (response) {
-                            console.log('Webhock response', response);
-                          })
-                          .catch(function (error) {
-                            console.log(error);
-                          });
+                            axios.post(process.env.WEBHOOK_URL, {
+                                content: hock_content,
+                                username: hock_name
+                            })
+                                .then(function (response) {
+                                    console.log('Webhock response', response);
+                                })
+                                .catch(function (error) {
+                                    console.log(error);
+                                });
+                        }
 
                         // handle ref link payout
-                        if(order.ref_address) {
+                        if (order.ref_address) {
                             console.log('order - reflink payout to:', order.ref_address)
                             let payoutObject = {
-                                address: order.ref_address, 
-                                value: 0, 
+                                address: order.ref_address,
+                                value: 0,
                                 message: 'Example reflink payout',
                                 tag: 'EINFACHIOTAMAGAZINEREFLINKPAYOUT'
-                              }
-                              paymentModule.payout.send(payoutObject)
-                              .then(result => {
-                                console.log("reflink payout done.", result)
-                              })
-                              .catch(err => {
-                                console.log(err)
-                              })
+                            }
+                            paymentModule.payout.send(payoutObject)
+                                .then(result => {
+                                    console.log("reflink payout done.", result)
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                })
                         }
 
                     }
@@ -89,16 +91,16 @@ Order.setPayed = function (order) {
 
 Order.setSent = function (order) {
     console.log('setOrderSent', order)
- 
-    Order.findOne({ _id: order._id }, function(err, order) {
-        if(!err) {
-            if(!order) {
+
+    Order.findOne({ _id: order._id }, function (err, order) {
+        if (!err) {
+            if (!order) {
                 // there is no order with this id
                 console.log('setOrderSent no order found.')
             } else {
                 order.status = 'sent';
-                order.save(function(err) {
-                    if(!err) {
+                order.save(function (err) {
+                    if (!err) {
                         console.log("order sent", order);
                     }
                     else {
