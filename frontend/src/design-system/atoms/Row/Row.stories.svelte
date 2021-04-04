464cdc567cbfbc757064b86a6814c480345ebd81
Sven Reiser <svreiser@gmail.com>
2021-04-05 00:28:54 +0200

    adds Row component

---
+++ frontend/src/design-system/atoms/Row/Row.stories.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Section from '../Section.svelte'
+  import Container from '../Container/Container.svelte'
+  import Row from './Row.svelte'
+</script>
+
+<Meta 
+  title="💧 Atoms / Row" 
+  component={Row} 
+  argTypes={{
+    class: { control: {
+      type: 'text',
+    } },
+  }}
+/>
+
+<Template let:args>
+  <Section dark>
+    <Container>
+      <Row {...args}>
+        Row #1
+      </Row>
+      <Row {...args}>
+        Row #2
+      </Row>
+    </Container>
+  </Section>
+</Template>
+
+<Story 
+  name="Row Layout" 
+/>
