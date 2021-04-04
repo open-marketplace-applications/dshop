324fbfa2aeac4a578098474440cdf79e057136f9
Sven Reiser <svreiser@gmail.com>
2021-04-05 01:55:00 +0200

    adds Countdown molecule

---
+++ frontend/src/design-system/molecules/Countdown/Countdown.stories.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Countdown from './Countdown.svelte'
+</script>
+
+<Meta 
+  title="🌱 Molecules / Countdown" 
+  component={Countdown} 
+  argTypes={{
+    countdown: { control: {
+      type: 'date',
+    } },
+    class: { control: {
+      type: 'text',
+    } },
+  }}
+/>
+
+<Template let:args>
+  <Countdown {...args} />
+</Template>
+
+<Story 
+  name="Countdown default"
+/>
