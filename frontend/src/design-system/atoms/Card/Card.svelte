8387de10bf6d2ea61016d17cb0717d48f86ec7b7
Sven Reiser <svreiser@gmail.com>
2021-04-04 19:44:17 +0200

    adds Card component

---
+++ frontend/src/design-system/atoms/Card/Card.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let padding: boolean = true
+</script>
+
+<div class={`card ${padding ? 'padding' : ''}`} >
+  <slot></slot>
+</div>
+
+<style lang="scss">
+  .card {
+    border-radius: var(--radius);
+    background-color: var(--color-element);
+
+    &.padding {
+      padding: var(--space-md);
+    }
+  }
+</style>
