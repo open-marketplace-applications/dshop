03eaf0fa248cf6c52e390ae3ad1a2c34cd786e3e
Sven Reiser <svreiser@gmail.com>
2021-04-04 22:42:33 +0200

    add Link component

--- frontend/src/design-system/atoms/Link/Link.svelte
+++ frontend/src/design-system/atoms/Link/Link.svelte
@@ -1,18 +1,26 @@
 <script lang="ts">
+	type Target = 'self' | 'blank' | 'parent'
 	export let link: string
 	export let text: string
+	export let target: Target = 'parent'
+	export let block: boolean = true
+
 </script>
 
-<a href={link} class={`link ${$$props.class}`}>
+<a 
+	href={link}
+	target={`_${target}`}
+	class={`link ${block ? 'block' : ''} ${$$props.class}`}
+>
 	<slot />
 	{text}
 </a>
 
 <style>
-	a.link {
+	.link {
 		opacity: 0.25;
 		transition: var(--transition);
-		display: block;
+		display: inline-block;
 		text-align: center;
 		margin-bottom: var(--space-sm);
 		font-size: var(--space-sm);
@@ -22,7 +30,11 @@
 		color: var(--color-text);
 	}
 
-	a.link:hover {
+	.link.block {
+		display: block;
+	}
+
+	.link:hover {
 		opacity: 1;
 	}
 </style>
