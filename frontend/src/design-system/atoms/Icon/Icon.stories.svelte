0a0db525b667b9182fece8d278d0e5ab3ad0aa06
Sven Reiser <svreiser@gmail.com>
2021-04-04 21:17:14 +0200

    adds Icon component

---
+++ frontend/src/design-system/atoms/Icon/Icon.stories.svelte
@@ -0,0 +1,61 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Icon from './Icon.svelte'
+</script>
+
+<Meta 
+  title="💧 Atoms / Icon" 
+  component={Icon} 
+  argTypes={{
+    icon: { control: {
+      type: 'select',
+      options: ['sun', 'moon', 'link', 'back', 'share']
+    } },
+    size: { control: {
+      type: 'select',
+      options: ['sm', 'md', 'lg', 'xl']
+    } },
+    class: { control: {
+      type: 'text',
+    } },
+  }}
+/>
+
+<Template let:args>
+  <Icon {...args} />
+</Template>
+
+<Story 
+  name="Sun" 
+  args={{
+    icon: 'sun'
+  }}
+/>
+
+<Story 
+  name="Moon" 
+  args={{
+    icon: 'moon'
+  }}
+/>
+
+<Story 
+  name="Back" 
+  args={{
+    icon: 'back'
+  }}
+/>
+
+<Story 
+  name="Share" 
+  args={{
+    icon: 'share'
+  }}
+/>
+
+<Story 
+  name="Link"
+  args={{
+    icon: 'link'
+  }}
+/>
