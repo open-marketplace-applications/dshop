61e3721e2eeba38eb29c8377cc3186acad9f1b8f
Sven Reiser <svreiser@gmail.com>
2021-04-04 20:40:46 +0200

    adds Form compnent

---
+++ frontend/src/design-system/atoms/Form/Form.stories.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Section from '../Section.svelte'
+  import Form from './Form.svelte'
+</script>
+
+<Meta 
+  title="💧 Atoms / Form" 
+  component={Form} 
+  argTypes={{
+    class: { control: {
+      type: 'text',
+    } },
+    onSubmit: { action: 'onClick' },
+  }}
+/>
+
+<Template let:args>
+  <Form  {...args} >
+    This is a pure style component with a callback. Your Inputs go here, see 🌱 Molecules / Subscribe as an example.
+  </Form>
+</Template>
+
+<Story 
+  name="Empty Form"
+/>
