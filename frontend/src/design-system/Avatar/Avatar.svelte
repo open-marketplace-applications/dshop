7bd035ee813e50df9e36fb51d3f40261b5d909af
Sven Reiser <svreiser@gmail.com>
2021-04-04 18:23:02 +0200

    updates avatar story to svelte

--- frontend/src/design-system/Avatar/Avatar.svelte
+++ frontend/src/design-system/Avatar/Avatar.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
-	type avatarSize = 'md' | 'sm' | 'lg'
+	type avatarSize = 'md' | 'sm' | 'lg' | 'xl'
 
 	export let imageUrl: string
 	export let size: avatarSize = 'md'
+
+	// TODO: Handle fallback if no image provided
 </script>
 
-<div class={`avatar-image ${size}`} style="background-image: url('{imageUrl}')" />
+<div 
+	class={`avatar-image ${size}`} 
+	style="background-image: url('{imageUrl}')"
+/>
 
 <style>
 	.avatar-image {
 		border-radius: 100%;
-		border: var(--border);
 		margin-right: var(--space-xs);
 		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
 	}
 
 	.avatar-image.sm {
@@ -26,15 +32,21 @@
 	}
 
 	.avatar-image.lg {
+		--dimension: calc(var(--space-xxl) + var(--space-xs));
+		width: var(--dimension);
+		height: var(--dimension);
+	}
+	.avatar-image.xl {
 		--dimension: calc(100vw - (var(--space-lg) * 2));
 		width: var(--dimension);
 		height: var(--dimension);
 	}
 
-	@media (min-width: 720px) {
-		.avatar-image.lg {
-			height: 400px;
-			width: 400px;
+	@media (min-width: 720px) {		
+		.avatar-image.xl {
+			--dimension: calc((var(--space-xxxl) * 5));
+			height: var(--dimension);
+			width: var(--dimension);
 		}
 	}
 </style>
