177b5d3882238b8981172cd25cdaabe997bbcf10
Sven Reiser <svreiser@gmail.com>
2021-04-05 15:43:41 +0200

    removes Message import

--- frontend/src/lib/Messages.svelte
+++ frontend/src/lib/Messages.svelte
@@ -1,38 +1,14 @@
 <script lang="ts">
-	import Message from '$lib/Message.svelte'
-
-	import { Form, Button } from '../design-system/index'
+  import SendMessage from './SendMessage.svelte'
   
 	export let socket
 	export let messages
-	
-  let new_message = ''
-	const sendMessage = () => {
-		console.log('sendMessage called 2:', new_message)
-		// socket.emit("new_message", new_message )
-        socket.emit("new_message", new_message, (data) => {
-            console.log(data); // data will be "woot"
-        });
-    console.log("socket", socket)
-	}
 </script>
 
 <h5 style="margin-top: var(--space-xl); margin-bottom: var(--space-sm)">Messages</h5>
-<Form>
-  <input 
-    type="text" 
-    placeholder="Drop a line" 
-    bind:value={new_message}
-  >
-  <Button 
-    callback={sendMessage} 
-    type='submit'
-  >
-    Send message
-  </Button>
-</Form>
+<SendMessage socket={socket} />
 {#each messages.reverse() as message, i}
-    <li>{message}</li>
+  <li>{message}</li>
   <!-- <Message
     type='user-connected' 
     author='@johnDoe'
---
+++ frontend/src/lib/SendMessage.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let socket
+
+  import { Form, Button } from '../design-system/index'
+
+  let new_message = ''
+	const sendMessage = () => {
+		console.log('sendMessage called 2:', new_message)
+		// socket.emit("new_message", new_message )
+        socket.emit("new_message", new_message, (data) => {
+            console.log(data); // data will be "woot"
+        });
+    console.log("socket", socket)
+	}
+</script>
+
+<Form>
+  <input 
+    type="text" 
+    placeholder="Drop a line" 
+    bind:value={new_message}
+  >
+  <Button 
+    callback={sendMessage} 
+    type='submit'
+  >
+    Send message
+  </Button>
+</Form>
