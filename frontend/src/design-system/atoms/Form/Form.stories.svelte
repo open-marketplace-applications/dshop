2843284a6e17f47fa4d0af39a26f0dcab25625a6
Sven Reiser <svreiser@gmail.com>
2021-04-06 19:41:06 +0200

    improves Form atom and story

--- frontend/src/design-system/atoms/Form/Form.stories.svelte
+++ frontend/src/design-system/atoms/Form/Form.stories.svelte
@@ -1,22 +1,53 @@
 <script>
   import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
   import Form from './Form.svelte'
+  import Input from '../Input/Input.svelte'
+  import Button from '../Button/Button.svelte'
+  $: user = {
+    name: 'Peter',
+    phone: '123',
+    email: 'abc@efg.com'
+  }
 </script>
 
 <Meta 
   title="💧 Atoms / Form" 
   component={Form} 
   argTypes={{
-    class: { control: {
-      type: 'text',
-    } },
     onSubmit: { action: 'onClick' },
   }}
 />
 
 <Template let:args>
-  <Form  {...args} >
-    This is a pure style component with a callback. Your Inputs go here, see 🌱 Molecules / Subscribe as an example.
+  <Form {...args} >
+    <h2>Example Form</h2>
+    <Input
+      type="text" 
+      name="name" 
+      label="Name" 
+      placeholder="Enter your name" 
+      bind:value={user.name} 
+    />
+    <Input 
+      type="text"
+      name="phone" 
+      label="Phone" 
+      placeholder="Enter your phone"
+      bind:value={user.phone} 
+    />
+    <Input
+      type="email"
+      name="email" 
+      label="Email" 
+      placeholder="Enter your email"
+      bind:value={user.email} 
+    />
+    <Button 
+      label="Send" 
+      type=submit
+      block
+      size=lg 
+    />
   </Form>
 </Template>
 
